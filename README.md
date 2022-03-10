90d40c74a002a00a1eae19a52ec1a10fa2b6dc29
walz0 <aidan.m.walz@gmail.com>
2022-03-10 00:59:20 -0500

    Added TBA library for api and automatic match submission

---
+++ README.md
@@ -0,0 +1,5 @@
+# frc-match-dl
+An adaptation of twitch-dl for easily downloading FRC clips from twitch vods
+
+# Dependencies
+- twitch-dl
---
+++ tba.py
@@ -0,0 +1,27 @@
+import requests
+import urllib.parse
+
+TBA_API_KEY = "Z37IOn5LR76k6oZX42Yj6qktALW6DNd1aoQMeUSGzf1EEq1Cf2yX9jJcjiiKGIDx"
+
+def submitVideo(match_key, url):
+    headers = {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/x-www-form-urlencoded",
+        "cookie": "ACSID=~AJKiYcGeegduQLHJXj5q2wQRn5_oadnPvzfOdUcA3NhNAb-ALPVlyGz8TL02bTm7Y4Hm5W9w5Zraz4aaEMlO6TZ7DlndOXQpiLOkVAtIjjgNNTxY0HS6T_YpiKi8ZSvsfzkGpBL368AecSnuDU44_OkCF5XoOOMOc9_x9J0GO_4m8uaxxa-kaWF1z9WCHHO_x9tKXWaeXgOZPFYyIOzTSAYU9DkZrCnwcUoWaaxTZLiw0OTCB4tBOy22_TPAM1zsJ-rFZ4caSHV6GHp4Y0_YE_W27XZMETOi8FxVtBeBN4P41qICI8ej6zcpK_eQ_Fgpx9bYkHtNEVnIoy_irIe84afxeoCKYMydFg; tba-favorite-teams={\"frc5484\":true}",
+        "origin": "https://www.thebluealliance.com"
+    }
+    youtube = urllib.parse.quote(url)
+    res = requests.post(
+        "http://www.thebluealliance.com/suggest/match/video?match_key={0}&youtube_url={1}".format(match_key, youtube), 
+        headers=headers
+    )
+
+def api(query):
+    headers = { "X-TBA-Auth-Key": TBA_API_KEY }
+    res = requests.get("https://www.thebluealliance.com/api/v3" + query, headers=headers)
+    return res.json()
+
+# print(api("/event/2022inkok/matches"))
+# submitVideo("2022inkok_sf1m2", "https://www.youtube.com/watch?v=h4grd3i8YfU")
